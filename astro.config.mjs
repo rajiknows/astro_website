2bb52d793c3ef9df025e9125f8dcb328d6d090cb
rajiknows <rajeshhjhamain@gmail.com>
2025-07-09 15:19:18 +0530

    feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [mdx(), sitemap(), tailwind()],
 });
